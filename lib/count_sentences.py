1ea5654e058ffeca08e01aef42ca1245e4889cfa
bonganimasemola <bonganimasemola@icloud.com>
2023-11-29 20:37:37 +0300

    counting sentences lad complete

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,43 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self._value = None  
+        self.value = value  
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if not isinstance(new_value, str):
+            print("The value must be a string.")
+        else:
+            self._value = new_value
+      
+      
+    def is_sentence(self):
+        result = self._value.endswith(".")
+        print(f"Value: {self._value}, Result: {result}")
+        return result
+    
+    def is_question(self):
+      return self._value.endswith("?")
+    
+    def is_exclamation(self):
+      return self._value.endswith("!")
+    
+    def count_sentences(self):
+      value = self.value
+      for punc in ['!','?']:
+        value = value.replace(punc, '.')
+    
+      sentences = [s for s in value.split('.') if s]
+    
+      return len(sentences)
+
+    
+      
+     
+  
